d9ae65899557997ab38f712c89f3331313fd665e
tsaltd <wp710012@gmail.com>
2016-03-19 14:40:17 -0400

    PreRunSetup

--- GeekQuiz/Startup.cs
+++ GeekQuiz/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using GeekQuiz.Models;
 using GeekQuiz.Services;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace GeekQuiz
 {
@@ -43,14 +45,21 @@ namespace GeekQuiz
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddApplicationInsightsTelemetry(Configuration);
-
             services.AddEntityFramework()
-                .AddSqlServer()
-                .AddDbContext<ApplicationDbContext>(options =>
-                    options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
+                    .AddSqlServer()
+                    .AddDbContext<ApplicationDbContext>(options =>
+             options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]))
+                    .AddDbContext<TriviaDbContext>(options =>
+             options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
+             services.AddApplicationInsightsTelemetry(Configuration);
+
+             services.AddEntityFramework()
+                     .AddSqlServer()
+                     .AddDbContext<ApplicationDbContext>(options =>
+                                options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
+
+             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
@@ -59,6 +68,12 @@ namespace GeekQuiz
             // Add application services.
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                options.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Ignore;
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -108,6 +123,8 @@ namespace GeekQuiz
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            SampleData.Initialize(app.ApplicationServices);
         }
 
         // Entry point for the application.
